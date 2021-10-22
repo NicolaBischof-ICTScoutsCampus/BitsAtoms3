48bfd4b7e13b6473c154fb7bb7f120fa90f15d7b
NicolaBischof-ICTScoutsCampus <nicola.bischof@ict-scouts.ch>
2021-10-22 13:30:40 +0200

    Added first diagram
    Inner circle diameter => actual max temp.
    Outer circle diameter => future max temp.

--- Ex01/sketch.js
+++ Ex01/sketch.js
@@ -5,10 +5,13 @@ let table;
 const canvasWidth = 600;
 const canvasHeight = 600;
 
+
+
 // https://p5js.org/reference/#/p5/loadTable
 function preload() {
   table = loadTable('future_cities_data_truncated.csv', 'csv', 'header');
 }
+
 
 function setup() {
   createCanvas(canvasWidth, canvasHeight);
@@ -19,5 +22,31 @@ function setup() {
 }
 
 function draw() {
-  background('#fae');
+  background('#000000');
+  noStroke();
+  var maxTempMonthFuture = table.getColumn('future_Max_Temperature_of_Warmest_Month');
+  let xVal = 100;
+  let yVal = 150;
+  fill(255,0,255);
+  maxTempMonthFuture.forEach(element => {
+    circle(xVal,yVal,int(element));
+    xVal += 50;
+    if(xVal >= 500){
+      yVal += 50;
+      xVal = 100;
+    }
+  });
+  xVal = 100;
+  yVal = 150;
+  fill(0,0,0);
+  var maxTempMonth = table.getColumn('Max_Temperature_of_Warmest_Month');
+  maxTempMonth.forEach(element => {
+    circle(xVal,yVal,int(element));
+    xVal += 50;
+    if(xVal >= 500){
+      yVal += 50;
+      xVal = 100;
+    }
+  });
+  
 }
