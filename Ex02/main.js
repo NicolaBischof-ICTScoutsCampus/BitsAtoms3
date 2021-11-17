a709b24d0ae2dceee84962a146bba468598c78ea
NicolaBischof-ICTScoutsCampus <nicola.bischof@ict-scouts.ch>
2021-11-17 14:19:01 +0100

    Refined the Labels and visibility
    Bigger Labels, Better Colors
    
    + Fixed reversed longitude

--- Ex02/main.js
+++ Ex02/main.js
@@ -29,7 +29,7 @@ async function init() {
     };
   });
 
-  const chartWidth = 5500;
+  const chartWidth = 6000;
   const chartHeight = 1080;
 
   const local = d3.local();
@@ -49,77 +49,156 @@ async function init() {
     .text("a simple tooltip");
 
   const selection = svg.selectAll("rect").data(simplifiedData).enter();
-  selection
-    .append("rect")
-    .attr("fill", "blue")
-    .attr("x", (value) => {
-      return scale(value.longitude, -180, 180, chartWidth, 0);
-    })
-    .attr("y", (value) => {
-      return scale(value.latitude, -90, 90, chartHeight, 0);
-    })
-    .attr("width", 10)
-    .attr("height", 10)
-    .attr("data-city", (value) => {
-      return `${value.currentCity}`;
-    });
 
+  // ------------------------------
+  // DIFFERENCE BETWEEN DATA POINTS
+  // ------------------------------
   selection
-    .append("rect")
-    .attr("fill", "red")
-    .attr("x", (value) => {
-      return scale(value.longitude, -180, 180, chartWidth, 0);
-    })
-    .attr("y", (value) => {
-      return scale(value.future_latitude, -90, 90, chartHeight, 0);
-    })
-    .attr("width", 10)
-    .attr("height", 10)
-    .attr("data-city", (value) => {
-      return `${value.currentCity}`;
-    });
-
-    selection
     .append("line")
-    .style("stroke", "grey")
-    .style("stroke-width", 1)
+    .style("stroke",  (value) => {
+      if(higherLower(value.latitude, value.future_latitude) == 0){
+        return "crimson";
+      }
+      else{
+        return "DodgerBlue"
+      }
+    })
+    .style("stroke-width", 3)
     .attr("x1", (value) => {
-      return scale(value.longitude, -180, 180, chartWidth, 0)+5;
+      return scale(value.longitude, -180, 180, 0, chartWidth) + 8;
     })
     .attr("y1", (value) => {
       return scale(value.latitude, -90, 90, chartHeight, 0);
     })
     .attr("x2", (value) => {
-      return scale(value.longitude, -180, 180, chartWidth, 0)+5;
+      return scale(value.longitude, -180, 180, 0, chartWidth) + 8;
     })
     .attr("y2", (value) => {
       return scale(value.future_latitude, -90, 90, chartHeight, 0);
+    })
+    .attr("data-city", (value) => {
+      return `${value.currentCity} difference: ${Math.round(
+        diff(value.latitude, value.future_latitude)
+      )} `;
     });
 
-  
-  //LONGITUTE 0
+  // ----------------
+  // CURRENT LATITUDE
+  // ----------------
+  selection
+    .append("rect")
+    .attr("fill", (value) => {
+      if(higherLower(value.latitude, value.future_latitude) == 0){
+        return "crimson";
+      }
+      else{
+        return "DodgerBlue"
+      }
+    })
+    .attr("x", (value) => {
+      return scale(value.longitude, -180, 180, 0, chartWidth);
+    })
+    .attr("y", (value) => {
+      return scale(value.latitude, -90, 90, chartHeight, 0);
+    })
+    .attr("width", 16)
+    .attr("height", 16)
+    .attr("data-city", (value) => {
+      return `${value.currentCity} now`;
+    });
+
+  // ---------------
+  // FUTURE LATITUDE
+  // ---------------
+  selection
+    .append("circle")
+    .attr("stroke",  (value) => {
+      if(higherLower(value.latitude, value.future_latitude) == 0){
+        return "crimson";
+      }
+      else{
+        return "DodgerBlue"
+      }
+    })
+    .attr("cx", (value) => {
+      return scale(value.longitude, -180, 180, 0, chartWidth) + 8;
+    })
+    .attr("cy", (value) => {
+      return scale(value.future_latitude, -90, 90, chartHeight, 0);
+    })
+    .attr("r", 8)
+    .attr("data-city", (value) => {
+      return `${value.currentCity} in the future`;
+    });
+
+  // ------------
+  // EQUATOR LINE
+  // ------------
   svg
     .append("line")
-    .style("stroke", "grey")
+    .style("stroke", "crimson")
     .style("stroke-width", 1)
     .attr("x1", 0)
     .attr("y1", chartHeight * 0.5)
     .attr("x2", chartWidth)
     .attr("y2", chartHeight * 0.5);
 
+  // ------------
+  // TOOLTIPS
+  // ------------
   svg
     .selectAll("rect")
     .on("mouseover", function () {
       const city = this.dataset.city;
       local.set(this, d3.select(this).attr("fill"));
-      d3.select(this).attr("fill", "pink");
+      d3.select(this).attr("fill", "white");
       // show tooltip
       d3.select("#tooltip").text(city).style("visibility", "visible");
     })
     .on("mousemove", function (event) {
       const coords = d3.pointer(event);
-      // uncomment to see current x and y mouse positions
-      console.log("x: ", coords[0], "y: ", coords[1]);
+
+      d3.select("#tooltip")
+        .style("left", coords[0] + 10 + "px")
+        .style("top", coords[1] - 10 + "px");
+    })
+    .on("mouseout", function () {
+      d3.select(this).attr("fill", local.get(this));
+      d3.select("#tooltip").style("visibility", "hidden");
+    });
+
+  svg
+    .selectAll("circle")
+    .on("mouseover", function () {
+      const city = this.dataset.city;
+      local.set(this, d3.select(this).attr("fill"));
+      d3.select(this).attr("fill", "white");
+      // show tooltip
+      d3.select("#tooltip").text(city).style("visibility", "visible");
+    })
+    .on("mousemove", function (event) {
+      const coords = d3.pointer(event);
+
+      d3.select("#tooltip")
+        .style("left", coords[0] + 10 + "px")
+        .style("top", coords[1] - 10 + "px");
+    })
+    .on("mouseout", function () {
+      d3.select(this).attr("fill", local.get(this));
+      d3.select("#tooltip").style("visibility", "hidden");
+    });
+
+  svg
+    .selectAll("line")
+    .on("mouseover", function () {
+      const city = this.dataset.city;
+      local.set(this, d3.select(this).attr("stroke"));
+      d3.select(this).attr("stroke", "black");
+      // show tooltip
+      d3.select("#tooltip").text(city).style("visibility", "visible");
+    })
+    .on("mousemove", function (event) {
+      const coords = d3.pointer(event);
 
       d3.select("#tooltip")
         .style("left", coords[0] + 10 + "px")
@@ -133,7 +212,27 @@ async function init() {
 
 init();
 
-//Small Processing Like Map Function
+// ------------
+// MAP FUNCTION
+// ------------
 function scale(number, inMin, inMax, outMin, outMax) {
   return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+}
+
+// -------------
+// DIFF FUNCTION
+// -------------
+function diff(num1, num2) {
+  if (num1 > num2) {
+    return num1 - num2;
+  } else {
+    return num2 - num1;
+  }
+}
+function higherLower(num1, num2) {
+  if (num1 > num2) {
+    return 0;
+  } else {
+    return 1;
+  }
 }
